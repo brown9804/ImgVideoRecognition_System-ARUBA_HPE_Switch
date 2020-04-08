5db1e0e5fce368c98a5babb50cd87b75610ac15d
Belinda Brown <belindabrownr04@gmail.com>
2020-04-08 13:24:36 -0600

    Update main
    Autor and general expl

--- Images_Recognition/main.py
+++ Images_Recognition/main.py
@@ -1,5 +1,15 @@
 #Python3
 
+
+##--------------------------------Main file------------------------------------
+##
+## Copyright (C) 2020 by Belinda Brown Ramírez (belindabrownr04@gmail.com)
+##	Image recognition system for diagnosis of a network switch
+##	
+##-----------------------------------------------------------------------------
+
+
+
 ##########  			IMPORTING PACKAGES   			##########
 
 import cv2
@@ -11,48 +21,36 @@ from collections import OrderedDict
 ##########  			DEFINITIONS OF NECESSARY FUNCTIONS   			##########
 
 ######					 FILTERING NOISE / MAKING THE IMAGE SHARP
-#####   Using Kittler's thresholding using the
-#####   tool previously created in  https://github.com/brown9804/Image_Segmentation_Project-
-#####   a variance of 1: 15.381974 is obtained, so values ​​are used 15 for images
-#####	since they are considered to be laboratory without changes in lighting
-#####	Considering the different tests, the values ​​are considered
-####	15 15 7 15 (30s approx) although
-#####	the bibliography read recommends values ​​of 10 10 7 21 (1:42 minutess approx)
-
 # def denoising_sharpening(input):
-#     sin_ruido= cv2.fastNlMeansDenoisingColored(input, None,15,15,7,15)
+#     without_noise= cv2.fastNlMeansDenoisingColored(input, None,15,15,7,15)
 #     kernel=np.array([[-1,-1,-1,-1,-1],
 #                       [-1,2,2,2,-1],
 #                       [-1,2,8,2,-1],
 #                       [-2,2,2,2,-1],
 #                       [-1,-1,-1,-1,-1]])/8.0
-#     sin_ruido = cv2.filter2D(sin_ruido,-1,kernel)
-#     return sin_ruido
+#     without_noise = cv2.filter2D(without_noise,-1,kernel)
+#     return without_noise
 
-###### We define a function that calculates the arithmetic mean of the elements of the list.
-###### Calculates the average value of all numbers, adds all and divides them by the total amount
+##########  				FOR TUPLE 		   			##########
 
-# def media_arit(list):
+# def ArithmeticAverageTUPLE(list):
 #     n = float(len(list))
 #     return tuple(sum(x[i] for x in list)/n for i in range (len(list[0])))
 
-#####	We define a function for variance between the elements of the list
-#####	the square of the standard deviation, defined below the standard deviation.
 
-# def varianza(list):
+# def varianceTUPLE(list):
 #     n = float(len(list))
 #     return tuple(sum(x[i] + (media_arit(list)-x[i])**2   for x in list)/n for i in range (len(list[0])))
 
 
-####   For a list
-#### Arithmetic average
+##########  				FOR LIST 		   			##########
 def ArithmeticAveragelist(list):
 	n = len(list)
 	sum = 0
 	for indice in range (0, n):
 		sum = sum + list[indice]
 	return sum/n
-####	Variance
+
 def variance_list(list):
 	n = len(list)
 	sum = 0
@@ -60,11 +58,6 @@ def variance_list(list):
 		sum = sum + (ArithmeticAveragelist(list)-list[indice])**2
 	return sum/n
 
-####	Standard deviation
-##	Standard deviation is the most common measure of dispersion, indicating how
-## scattered the data is with respect to average. The greater the standard deviation,
-##	the greater will be the dispersion of the data. We need it because it doesn't
-## consider very abrupt changes in pixel values, as well as their location greater than zero.
 def StandarDesv(list):
 	desvi = variance_list(list)**(1/2)
 	return desvi
@@ -139,20 +132,8 @@ for f1 in files:
 	data.append(img)
 	#####	uses a gray filter for easy recognition
 	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY, 0)
+	
 	########		 COMPARING THE IMAGE USING TEMPLATE METHOD 			########
-	#####	Template Matching is a method to search and find the location of
-	###### a template image into a larger image. OpenCV comes with a
-	###### function cv2.matchTemplate () for this purpose. Just slide
-	###### the template image over the input image (as in 2D convolution)
-	###### and compare the template and patch of the input image below the image
-	###### Of the template. Various comparison methods are implemented in OpenCV.
-	##### Return a grayscale image, where each pixel indicates how much
-	##### match the neighborhood of that pixel with the template.
-
-	##### TM_CCOEFF_NORMED does Correlation coefficient: the method is simply used to
-	##### a) make the template and image zero and
-	##### b) make the dark parts of the image negative values
-	##### and the bright parts of the positive values ​​of the image.
 	res_matching_green = cv2.matchTemplate(img_gray,template_green,cv2.TM_CCOEFF_NORMED)
 	res_matching_orange = cv2.matchTemplate(img_gray,template_orange,cv2.TM_CCOEFF_NORMED)
 	res_matching_dark_orange = cv2.matchTemplate(img_gray,template_dark_orange,cv2.TM_CCOEFF_NORMED)
@@ -163,14 +144,11 @@ for f1 in files:
 
 	###### Announces every time an image is reviewed
 	print("Image loaded, analyzing patterns ...")
-	###### gets the position
-	######-- theoretically
-	###### Returns elements chosen from x or y depending on the condition.
-	###### gives location x, and separated from all meet the condition
+	###### gets the position of matching 
 	location_green = np.where(res_matching_green >= threshold)
 	location_orange = np.where(res_matching_orange >= threshold)
 	location_dark_orange = np.where(res_matching_dark_orange >= threshold)
-							########## IF THERE IS GREEN THEN ... ##########
+				########## IF THERE IS GREEN THEN ... ##########
 	####### GREEN BEFORE FILTERING for X - without repeats
 	if len(location_green[0]) > 0:
 		for itergreenx in sorted(location_green[0]):
@@ -196,80 +174,51 @@ for f1 in files:
 		Y_Green_before_filtered.pop(0)
 		######	The deleted coordinate is added to the result
 		Y_Green_Filtered.append(yGreen0)
-		#####	To automate the filtering, the dispersion measurements are calculated
+		#####	To automate the filtering, the dispersion MEASUREMENTS are calculated
+		### For green x
 		X_ArithmeticAverage_Green = ArithmeticAveragelist(Geen_x)
-		# print("Arithmetic Average x Green", X_ArithmeticAverage_Green)
 		X_Variance_Green = variance_list(Geen_x)
-		# print("Variance x Green", X_Variance_Green)
 		X_StandarDesv_Green = StandarDesv(Geen_x)
-		# print("Standard Desvx Green", X_StandarDesv_Green)
+		#For green y 
 		Y_ArithmeticAverage_Green = ArithmeticAveragelist(Green_y)
-		# print("Arithmetic Average y Green", Y_ArithmeticAverage_Green)
 		Y_Variance_Green = variance_list(Green_y)
-		# print("Variance y Green", Y_Variance_Green)
 		Y_StandarDesv_Green = StandarDesv(Green_y)
-		# print("Standard Desv y Verde", Y_StandarDesv_Green)
-		# print("Green x complete ", Geen_x)
-		# print('Green y complete', Green_y)
-				########## 						GREEN	COORDENATES FILTERS  									##########
-		#Filter for X coordinate, because it locates pixels with similar threshold in the near area
-		# That is to say 1678,1679,1680 are 3 consecutive pixels so I would draw 3 rectangles
-		#Then for a group of similar data we need a coordinate. Since they don't change as much between
-		#groups is indifferent if the first one is not obtained, since the second meets the needs.
+			##########   GREEN COORDENATES FILTERS ONE FOR LOCATION								##########
+		#For green X
 		for e, i in sorted(zip(Geen_x, X_Green_before_filtered)):
+			#Difference between x coordinates
 			Diff_X_Green = i - e
-			# print(i)
-			# print(e)
-			# print("Diff x Green ", Diff_X_Green, i)
 			if X_StandarDesv_Green < Diff_X_Green:
 				X_Green_Filtered.append(i)
 		X_Green_Filtered = list(OrderedDict.fromkeys(X_Green_Filtered))
-		# Filter for the Y coordinate, because it locates pixels with a similar threshold in the near area
-		# That is,  144,145,146  are 3 consecutive pixels, so I would draw 3 rectangles
-		# Then for a group of similar data we need a coordinate. Since they don't change as much between
-		# groups is indifferent if the first is not obtained, given that the second meets the needs.
+		# Filter for the Y 
 		for ee, ii in sorted(zip(Green_y, Y_Green_before_filtered)):
+			#Difference between y coordinates
 			Diff_Y_Green = ii - ee
-			# print(ii)
-			# print(ee)
-			# print("Diff_Y_Green", Diff_Y_Green, ii)
 			if Y_ArithmeticAverage_Green - Y_StandarDesv_Green < Diff_Y_Green:
 				Y_Green_Filtered.append(ii)
 		Y_Green_Filtered = list(OrderedDict.fromkeys(Y_Green_Filtered))
-		#### Filter for the X coordinate, because it locates pixels with a similar threshold in the near area
-		#### Now to be able to order it again in coordinates we need an nxn array
-		#### Considering the filtering logic and the architecture at the port level in the switch
-		#### We know that given the necessary specifications, there may be a deviation angle, considering
-		##### this and the structure of the ports.
+		#Counting the number of pixels each coordinate 
 		number_X_Green = len(X_Green_Filtered)
 		number_Y_Green = len(Y_Green_Filtered)
-		# print("ny" , number_Y_Green)
-		# print("nx", number_X_Green)
 		HowManyGreen = 0
+		#Equalizing in order the number of pixels Y
 		while HowManyGreen < number_Y_Green-1:
 			HowManyGreen = HowManyGreen +1
-			# print(HowManyGreen, X_Green_Filtered)
 			if number_X_Green != number_Y_Green and number_X_Green < number_Y_Green:
 				X_Green_Filtered.append(xGreen0)
-		# print("X_Green_Filtered", X_Green_Filtered)
-		# print("Y_Green_Filtered", Y_Green_Filtered)
 		######		Joining the two x, y coordinates
 		Green_filtered_coordenates = sorted(zip(Y_Green_Filtered, X_Green_Filtered))
-		######	zip location return  example:  <zip object at 0x11cf638c0> pack and acces
-		####	zip () con n arguments, then the function will return an iterator that generates tuples of length n.
-		#####	Print points as (x,y)
-		# print(" Coordinate pairs for Green_filtered point ",Green_filtered_coordenates)
 		###### 	Draw the rectangle in that case where it finds green
 		for ptGreen in Green_filtered_coordenates:
-			####	cv2.rectangle(image where draw, place , color, thick line drawing)
-			###		color BGR
+			####	cv2.rectangle(image where draw, place , color BGR, thick line drawing)
 			cv2.rectangle(img, ptGreen, (ptGreen[0] + w_green, ptGreen[1] + h_green), (0,255,255), 4)
 			###	In this function the color goes BGR, what it does is put the text where it found the led
 			cv2.putText(img, 'GREEN', ptGreen, cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 4)
 			##### 	Count the number of LEDs you found in this state
 			Quantity_Leds_Green = Quantity_Leds_Green +1
 		print("The number of LEDs in Green Green status (on / on) found is:      ", Quantity_Leds_Green)
-				########## 						YELLOW ORANGE	COORDENATES FILTERS  									##########
+				########## IF THERE IS YELLOW ORANGE THEN ... ##########
 	if len(location_orange[0]) > 0:
 		##### for x on yellow orange before filtered
 		for iteryelloworangX in sorted(location_orange[0]):
@@ -295,30 +244,21 @@ for f1 in files:
 		Y_YellowOrange_before_filtered.pop(0)
 		###		The deleted coordinate is added to the result
 		Y_YellowOrange_Filtered.append(yYellowOrang0)
-		####	To automate the filtering, the dispersion measurements are calculated
+		####	To automate the filtering, the dispersion MEASUREMENTS are calculated
+		#For X
 		X_ArithmeticAverage_YellowOrange = ArithmeticAveragelist(YellowOrange_x)
-		# print("Arithmetic Average Yellow Orange X ", X_ArithmeticAverage_YellowOrange)
 		X_Variance_YellowOrange = variance_list(YellowOrange_x)
-		# print("Variance Yellow Orange X ", X_Variance_YellowOrange)
 		X_StandarDesv_YellowOrange = StandarDesv(YellowOrange_x)
-		# print("Standard Desv Amarillo Naranja X ", X_StandarDesv_YellowOrange)
+		#For Y
 		Y_ArithmeticAverage_YellowOrange = ArithmeticAveragelist(YellowOrange_y)
-		# print("Arithmetic Average Yellow Orange Y", Y_ArithmeticAverage_YellowOrange)
 		Y_Variance_YellowOrange = variance_list(YellowOrange_y)
-		# print("Variance Yellow Orange Y", Y_Variance_YellowOrange)
 		Y_StandarDesv_YellowOrange = StandarDesv(YellowOrange_y)
-		# print("Standard Desv Yellow Orange Y ", Y_StandarDesv_YellowOrange)
-		# print("Yellow Orange x complete ", YellowOrange_x)
-		# print('Yellow Orange y complete', YellowOrange_y)
-
 		#### Applying the same method for filter similar X COORDENATES
 		for eee,iii in sorted(zip(YellowOrange_x,X_YellowOrange_before_filtered)):
 			Diff_X_YellowOrnge = iii - eee
-			# print("Diff x Yellow Orange", Diff_X_YellowOrnge, iii)
 			if X_StandarDesv_YellowOrange<Diff_X_YellowOrnge:
 				X_YellowOrange_Filtered.append(iii)
 		X_YellowOrange_Filtered = list(OrderedDict.fromkeys(X_YellowOrange_Filtered))
-
 		#### Applying the same method for filter similar Y COORDENATES
 		for eeee,iiii in sorted(zip(YellowOrange_y,Y_YellowOrange_before_filtered)):
 			Diff_Y_YellowOrnge = iiii - eeee
@@ -331,18 +271,13 @@ for f1 in files:
 		HowManyYellowOrange = 0
 		while HowManyYellowOrange < number_Y_YellowOrange-1 :
 			HowManyYellowOrange = HowManyYellowOrange +1
-			# print(HowManyYellowOrange, X_YellowOrange_Filtered)
 			if number_X_YellowOrange != number_Y_YellowOrange and number_X_YellowOrange < number_Y_YellowOrange:
 				X_YellowOrange_Filtered.append(xYellowOrang0)
-
 		######		Joining the two x, y coordinates
 		YellowOrange_filtered_coordenates = sorted(zip(Y_YellowOrange_Filtered, X_YellowOrange_Filtered ))
-		#####	Print points as (x,y)
-		# print(" Coordinate pairs for Green_filtered point ",Green_filtered_coordenates)
 		###### 	Draw the rectangle in that case where it finds yellow orange
 		for ptYellowOrange in YellowOrange_filtered_coordenates:
-			####	cv2.rectangle(image where draw, place , color, thick line drawing)
-			###		color BGR
+			####	cv2.rectangle(image where draw, place , color BGR, thick line drawing)
 			#Draw a rectangle around the adapted region found in this case
 			cv2.rectangle(img, ptYellowOrange, (ptYellowOrange[0] + w_orange, ptYellowOrange[1] + h_orange), (0,255,255), 4)
 			###	In this function the color goes BGR, what it does is put the text where it found the led
@@ -350,7 +285,7 @@ for f1 in files:
 			##### 	Count the number of LEDs you found in this state
 			Quantity_Leds_YellowOrange = Quantity_Leds_YellowOrange +1
 		print("The number of LEDs in Yellow Orange state (on / problem) found is:      ", Quantity_Leds_YellowOrange)
-				########## 						DARK ORANGE	COORDENATES FILTERS  									##########
+				########## IF THERE IS ORANGE ORANGE OR DARK ORANGE (IS THE SAME) THEN  ... ##########
 	if len(location_dark_orange[0]) > 0:
 		#####	For x on yellow orange before filtered
 		for iterdarkorangX in sorted(location_dark_orange[0]):
@@ -376,30 +311,21 @@ for f1 in files:
 		Y_OrangeOrange_before_filtered.pop(0)
 		###		The deleted coordinate is added to the result
 		Y_OrangeOrange_Filtered.append(yOrangOrang0)
-		####	To automate the filtering, the dispersion measurements are calculated
+		####	To automate the filtering, the dispersion MEASUREMENTS are calculated
+		#	For X
 		X_ArithmeticAverage_OrangOrang = ArithmeticAveragelist(OrangeOrange_x)
-		# print("Arithmetic Average Orange Orange same as Dark Orange X", X_ArithmeticAverage_OrangOrang)
 		X_Variance_OrangOrang = variance_list(OrangeOrange_x)
-		# print("Variance Orange Orange same as Dark Orange X ", X_Variance_OrangOrang)
 		X_StandarDesv_OrangOrang = StandarDesv(OrangeOrange_x)
-		# print("Standard Desv same as Dark Orange X ", X_StandarDesv_OrangOrang)
+		#	For Y
 		Y_ArithmeticAverage_OrangOrang = ArithmeticAveragelist(OrangeOrange_y)
-		# print("rithmetic Average Orange Orange same as Dark Orange Y ", Y_ArithmeticAverage_OrangOrang)
 		Y_Variance_OrangOrang = variance_list(OrangeOrange_y)
-		# print("VVariance Orange Orange same as Dark Orange Y", Y_Variance_OrangOrang)
 		Y_desvacion_estandar_NN = StandarDesv(OrangeOrange_y)
-		# print(""Standard Desvr Orange Orange same as x Dark Orange Y ", Y_desvacion_estandar_NN)
-		# print("Orange Orange same as x Dark Orange complete ",  OrangeOrange_x)
-		# print("Orange Orange same as y Dark Orange complete", OrangeOrange_y)
-
 		#### Applying the same method for filter similar X COORDENATES
 		for nne,nni in sorted(zip(OrangeOrange_x,X_OrangeOrange_before_filtered)):
 			Diff_X_OrangOrang = nni - nne
-			# print("Diff Orange Orange ", Diff_X_OrangOrang, nni)
 			if X_StandarDesv_OrangOrang<Diff_X_OrangOrang:
 				X_OrangeOrange_Filtered.append(nni)
 		X_OrangeOrange_Filtered = list(OrderedDict.fromkeys(X_OrangeOrange_Filtered))
-
 		#### Applying the same method for filter similar Y COORDENATES
 		for nnee,nnii in sorted(zip(OrangeOrange_y,Y_OrangeOrange_before_filtered)):
 			Diff_Y_OrangOrang = nnii - nnee
@@ -412,16 +338,13 @@ for f1 in files:
 		HowManyDarkOrange = 0
 		while HowManyDarkOrange < number_Y_OrangOrang-1 :
 			HowManyDarkOrange = HowManyDarkOrange +1
-			# print(HowManyDarkOrange, X_OrangeOrange_Filtered)
 			if number_X_OrangOrang != number_Y_OrangOrang and number_X_OrangOrang < number_Y_OrangOrang:
 				X_OrangeOrange_Filtered.append(xOrangOrang0)
-
 		######		Joining the two x, y coordinates
 		OrangeOrange_filtered_coordenates = sorted(zip(Y_OrangeOrange_Filtered, X_OrangeOrange_Filtered))
 		###### 	Draw the rectangle in that case where it finds orange orange
 		for ptOrangOrang in OrangeOrange_filtered_coordenates:
-			####	cv2.rectangle(image where draw, place , color, thick line drawing)
-			###		color BGR
+			####	cv2.rectangle(image where draw, place , color BGR, thick line drawing)
 			#Draw a rectangle around the adapted region found in this case
 			cv2.rectangle(img, ptOrangOrang, (ptOrangOrang[0] + w_dark_orange, ptOrangOrang[1] + h_dark_orange), (0,255,255), 4)
 			###	In this function the color goes BGR, what it does is put the text where it found the led
