1fd2157237825b8668528a7b9a5e7350ddd5d3b3
Belinda Brown <belindabrownr04@gmail.com>
2020-05-30 00:06:44 -0600

    Ready with locs

--- Images_Recognition/main.py
+++ Images_Recognition/main.py
@@ -31,19 +31,41 @@ from collections import OrderedDict
 #     without_noise = cv2.filter2D(without_noise,-1,kernel)
 #     return without_noise
 
+#### For list items
+def mean_arit_list(list):
+	n = len(list)
+	sum = 0
+	for ind in range (0, n):
+		sum = sum + list[ind]
+	return sum/n
+
+def varnc_list(list):
+	n = len(list)
+	sum = 0
+	for ind in range (0, n):
+		sum = sum + (mean_arit_list(list)-list[ind])**2
+	return sum/n
+
+def stddesv_list(list):
+	desvi = varnc_list(list)**(1/2)
+	return desvi
 
 ######	Read the template
 template_green = cv2.imread('',0)
 template_orange = cv2.imread('',0)
 template_dark_orange = cv2.imread('',0)
+template_port = cv2.imread('',0)
 
 ######	Store the width (w) and height (h) of the template
 w_green, h_green = template_green.shape[::-1]
 w_orange, h_orange = template_orange.shape[::-1]
 w_dark_orange, h_dark_orange = template_dark_orange.shape[::-1]
+w_port, h_port = template_port.shape[::-1]
 
 ######	Specifying (threshold)
 threshold= 0.90
+thresholdport= 0.70
+
 ######	Directory with images verify
 img_dir = ''
 data_path = os.path.join(img_dir,'*.jpg')
@@ -93,10 +115,27 @@ for f1 in files:
 	OrangeOrange_filtered_coordenates = []
 	######	Number of LEDs in state # XXX
 	Quantity_Leds_OrangeOrange = 0
-
+					#######    FOR PORT     #####
+	port_x = []
+	port_x_bff = []
+	# 		Port compare Y values
+	port_y = []
+	port_y_bff = []
+	# 		Port coordenates filtered
+	port_x_f =[]
+	port_y_f =[]
+	## Join the coordenates
+	port_fil = []
+	# Count ports
+	count_port = 0
+	
 	##### Read each image
 	img = cv2.imread(f1)
 	print("\n", f1) #picture name
+	frames = cv2.imread(f1,0)
+	w_img, h_img = frames.shape[::-1]
+
+	
 	##### Store image data
 	data.append(img)
 	#####	uses a gray filter for easy recognition
@@ -106,6 +145,7 @@ for f1 in files:
 	res_matching_green = cv2.matchTemplate(img_gray,template_green,cv2.TM_CCOEFF_NORMED)
 	res_matching_orange = cv2.matchTemplate(img_gray,template_orange,cv2.TM_CCOEFF_NORMED)
 	res_matching_dark_orange = cv2.matchTemplate(img_gray,template_dark_orange,cv2.TM_CCOEFF_NORMED)
+	res_matching_port = cv2.matchTemplate(img_gray,template_port,cv2.TM_CCOEFF_NORMED)
 
 	##### If you use denoising image:
 	#f1Filtrada = cv2.imread(f1)
@@ -117,6 +157,7 @@ for f1 in files:
 	location_green = np.where(res_matching_green >= threshold)
 	location_orange = np.where(res_matching_orange >= threshold)
 	location_dark_orange = np.where(res_matching_dark_orange >= threshold)
+	location_port = np.where(res_matching_port >= thresholdport)
 
 				########## IF THERE IS GREEN THEN ... ##########
 	####### GREEN BEFORE FILTERING for Y - without repeats
@@ -297,6 +338,102 @@ for f1 in files:
 			##### 	Count the number of LEDs you found in this state
 			Quantity_Leds_OrangeOrange = Quantity_Leds_OrangeOrange + 1
 		print("The number of LEDs in Orange Orange status (problem / problem) found is:     ", Quantity_Leds_OrangeOrange)
+	
+	####    				IF PORT EXIST ...
+	if len(location_port[0]) > 0:
+		##### Ports X coordenates without repeats
+		for i in sorted(location_port[0]):
+			if i not in port_x:
+				port_x.append(i)
+		#### Before filtered x coordinate
+		port_x_bff =  port_x.copy()
+		#### Obtaining X first coordinate
+		x0 = port_x[0]
+		#### Deleting the first coordinate
+		port_x_bff.pop(0)
+		# Append the firt coordinate deleted before to the coordenates filtered
+		port_x_f.append(x0)
+		#### Before filtered y coordinate
+		for j in sorted(location_port[1]):
+			if j not in port_y:
+				port_y.append(j)
+		#### Before filtered y coordinate
+		port_y_bff =  port_y.copy()
+		#### Obtaining Y first coordinate
+		y0 = port_y[0]
+		#### Deleting the first coordinate
+		port_y_bff.pop(0)
+		# Append the firt coordinate deleted before to the coordenates filtered
+		port_y_f.append(y0)
+		#### To automate the filtering, the dispersion measures are calculated
+		port_x_mean = mean_arit_list(port_x)
+		#print("Port mean x coordinates		", port_x_mean)
+		port_x_var = varnc_list(port_x)
+		#print("Port variance x coordinates 	", port_x_var)
+		port_x_stdesv = stddesv_list(port_x)
+		#print("Port standard deviation x coordinates 	", port_x_stdesv)
+		port_y_mean = mean_arit_list(port_y)
+		port_y_stdesv = stddesv_list(port_y)
+		#### Applying the same method for filter similar X COORDENATES
+		for nne,nni in sorted(zip(port_x,port_x_bff)):
+			sub_port_x = nni - nne
+			if abs(port_x_stdesv) <abs(sub_port_x):
+				port_x_f.append(nni)
+		port_x_f = list(OrderedDict.fromkeys(port_x_f))
+		#### Applying the same method for filter similar Y COORDENATES
+		for nnee,nnii in sorted(zip(port_y,port_y_bff)):
+			Resta_Y_NN = nnii - nnee
+			if abs(port_y_mean - port_y_stdesv) < abs(Resta_Y_NN):
+				port_y_f.append(nnii)
+		port_y_f = list(OrderedDict.fromkeys(port_y_f))
+		##### Counting the total of coordinates finally filtered
+		count_port_x = len(port_x_f)
+		count_port_y = len(port_y_f)
+		count_p = 0
+		while count_p < count_port_y-1 :
+			count_p = count_p +1
+			if count_port_x != count_port_y and count_port_x < count_port_y:
+				port_x_f.append(x0)
+		loc_leds = []
+		loc_to_add = 0
+		for i in port_y_f:
+			loc_to_add = i + w_port -100
+			loc_leds.append(loc_to_add)
+		loc_leds = list(OrderedDict.fromkeys(loc_leds))
+		loc_full_port = []
+		# Concatenate port positions
+		loc_full_port = sorted(port_y_f + loc_leds )
+		# Port_fil will have the coordenates as ordered pairs
+		port_fil = sorted(zip(port_y_f, port_x_f))
+		# Drawing ports id on images
+		for pix_port in port_fil:
+			#### Rectangle and colors as color BGR
+			cv2.rectangle(img, pix_port, (pix_port[0] + w_port, pix_port[1] + h_port), (0,255,255), 5)
+			#### Label on drawing
+			cv2.putText(img, 'Port ', pix_port, cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 5)
+			###  Count port template, in each template are two ports. Thats why count_port*2
+			count_port = count_port + 1
+		print("The number of ports are:      ", count_port*2)
+		metic = (w_img-h_img)/count_port
+
+	loc_full_port = sorted(list(OrderedDict.fromkeys(loc_full_port)))
+	leds_on_fnd = sorted(list(OrderedDict.fromkeys(X_YellowOrange_Filtered + X_Green_Filtered + X_OrangeOrange_Filtered)))
+
+	print("Positions of the leds ON found", leds_on_fnd)
+	number_label = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11 ,12]
+	loc_led_templ = sorted(zip(loc_full_port,number_label))
+	diff = [] # For the result of the sub created for filter positions
+	for i in sorted(loc_led_templ):
+		for j in sorted(leds_on_fnd):
+			z = j + 20
+			x = i[0] - z
+			if x in range(-110,60):
+				if j in X_Green_Filtered:
+					print("Port", i[1], "statuts:						Green")
+				elif j in X_YellowOrange_Filtered:
+					print("Port", i[1], "statuts:						Orange")
+				elif j in X_OrangeOrange_Filtered:
+					print("Port", i[1], "statuts:						Dark Orange")
 	#### Shows me the figure already analyzed
 	cv2.imshow("\nProcessed Image",img)
 	### Since there are several, wait until you press a key and thus analyze the other image
