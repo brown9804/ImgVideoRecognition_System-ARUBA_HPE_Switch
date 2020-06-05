f1ab2dd3234262707ea8e9b9e0d96a0e8de63276
Belinda Brown <belindabrownr04@gmail.com>
2020-06-05 03:04:36 -0600

    Final Update image
    Remember change paths

--- Images_Recognition/main.py
+++ Images_Recognition/main.py
@@ -50,8 +50,8 @@ def stddesv_list(list):
 	desvi = varnc_list(list)**(1/2)
 	return desvi
 
-def color_filter(colorlocation, w_color, h_color, imagen, color):
-					#######    FOR COLOR    #####
+def color_filter(colorlocation, w_color, h_color, image, color):
+			#######    FOR COLOR    #####
 	######	COMPARING  X
 	color_x = []
 	y_color_bf = []
@@ -114,38 +114,16 @@ def color_filter(colorlocation, w_color, h_color, imagen, color):
 		color_flrd_cor = sorted(zip(x_color_f, y_color_f))
 		for ptcolor in color_flrd_cor:
 			####	cv2.rectangle(image where draw, place , color BGR, thick line drawing)
-			cv2.rectangle(imagen, ptcolor, (ptcolor[0] + w_color, ptcolor[1] + h_color), (0,255,255), 4)
+			cv2.rectangle(image, ptcolor, (ptcolor[0] + w_color, ptcolor[1] + h_color), (0,255,255), 4)
 			###	In this function the color goes BGR, what it does is put the text where it found the led
-			cv2.putText(imagen, str(color), ptcolor, cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 4)
+			cv2.putText(image, str(color), ptcolor, cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 4)
 			##### 	Count the number of LEDs you found in this state
 			num_leds_color = num_leds_color +1
 		print("The number of LEDs in " + color + " status (on / on) found is:      ", num_leds_color)
 		return x_color_f
 
-######	Read the template
-template_green = cv2.imread('',0)
-template_orange = cv2.imread('',0)
-template_dark_orange = cv2.imread('',0)
-template_port = cv2.imread('',0)
-
-######	Store the width (w) and height (h) of the template
-w_green, h_green = template_green.shape[::-1]
-w_orange, h_orange = template_orange.shape[::-1]
-w_dark_orange, h_dark_orange = template_dark_orange.shape[::-1]
-w_port, h_port = template_port.shape[::-1]
-
-######	Specifying (threshold)
-threshold= 0.90
-thresholdport= 0.70
-
-######	Directory with images verify
-img_dir = ''
-data_path = os.path.join(img_dir,'*.jpg')
-files = glob.glob(data_path)
-data = []
-######	Analyzing all the images in the folder
-for f1 in files:
-					#######    FOR PORT     #####
+def port_filter(port_location, w_p, h_p, image, name):
+		#######    FOR PORT     #####
 	port_x = []
 	port_x_bff = []
 	# 		Port compare Y values
@@ -158,59 +136,11 @@ for f1 in files:
 	port_fil = []
 	# Count ports
 	count_port = 0
-
-	##### Read each image
-	img = cv2.imread(f1)
-	print("\n", f1) #picture name
-	frames = cv2.imread(f1,0)
-	w_img, h_img = frames.shape[::-1]
-
-	##### Store image data
-	data.append(img)
-	#####	uses a gray filter for easy recognition
-	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY, 0)
-
-	########		 COMPARING THE IMAGE USING TEMPLATE METHOD 			########
-	res_matching_green = cv2.matchTemplate(img_gray,template_green,cv2.TM_CCOEFF_NORMED)
-	res_matching_orange = cv2.matchTemplate(img_gray,template_orange,cv2.TM_CCOEFF_NORMED)
-	res_matching_dark_orange = cv2.matchTemplate(img_gray,template_dark_orange,cv2.TM_CCOEFF_NORMED)
-	res_matching_port = cv2.matchTemplate(img_gray,template_port,cv2.TM_CCOEFF_NORMED)
-
-	##### If you use denoising image:
-	#f1Filtrada = cv2.imread(f1)
-	#img = denoising_sharpening(f1Filtrada)
-
-	###### Announces every time an image is reviewed
-	print("Image loaded, analyzing patterns ...")
-	###### Gets the position of matching
-	location_green = np.where(res_matching_green >= threshold)
-	location_orange = np.where(res_matching_orange >= threshold)
-	location_dark_orange = np.where(res_matching_dark_orange >= threshold)
-	location_port = np.where(res_matching_port >= thresholdport)
-	# Calling functions
-	X_Green_Filtered0 = color_filter(location_green, w_green, h_green, img, 'GREEN')
-	X_YellowOrange_Filtered0 = color_filter(location_orange, w_orange, h_orange, img, 'ORANGE')
-	X_OrangeOrange_Filtered0 = color_filter(location_dark_orange, w_dark_orange, h_dark_orange, img, 'DARK ORANGE')
-	# Checking empty
-	if X_Green_Filtered0:
-		X_Green_Filtered = list(OrderedDict.fromkeys(X_Green_Filtered0))
-	else:
-		X_Green_Filtered = []
-
-	if X_YellowOrange_Filtered0:
-		X_YellowOrange_Filtered = list(OrderedDict.fromkeys(X_YellowOrange_Filtered0))
-	else:
-		X_YellowOrange_Filtered = []
-
-	if X_OrangeOrange_Filtered0:
-		X_OrangeOrange_Filtered = list(OrderedDict.fromkeys(X_OrangeOrange_Filtered0))
-	else:
-		X_OrangeOrange_Filtered = []
-
+	
 	####    				IF PORT EXIST ...
-	if len(location_port[0]) > 0:
+	if len(port_location[0]) > 0:
 		##### Ports X coordenates without repeats
-		for i in sorted(location_port[0]):
+		for i in sorted(port_location[0]):
 			if i not in port_x:
 				port_x.append(i)
 		#### Before filtered x coordinate
@@ -222,7 +152,7 @@ for f1 in files:
 		# Append the firt coordinate deleted before to the coordenates filtered
 		port_x_f.append(x0)
 		#### Before filtered y coordinate
-		for j in sorted(location_port[1]):
+		for j in sorted(port_location[1]):
 			if j not in port_y:
 				port_y.append(j)
 		#### Before filtered y coordinate
@@ -265,7 +195,7 @@ for f1 in files:
 		loc_leds = []
 		loc_to_add = 0
 		for i in port_y_f:
-			loc_to_add = i + w_port -100
+			loc_to_add = i + w_p -100
 			loc_leds.append(loc_to_add)
 		loc_leds = list(OrderedDict.fromkeys(loc_leds))
 		loc_full_port = []
@@ -276,15 +206,91 @@ for f1 in files:
 		# Drawing ports id on images
 		for pix_port in port_fil:
 			#### Rectangle and colors as color BGR
-			cv2.rectangle(img, pix_port, (pix_port[0] + w_port, pix_port[1] + h_port), (0,255,255), 5)
+			cv2.rectangle(image, pix_port, (pix_port[0] + w_p, pix_port[1] + h_p), (0,255,255), 5)
 			#### Label on drawing
-			cv2.putText(img, 'Port ', pix_port, cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 5)
+			cv2.putText(image, str(name), pix_port, cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 5)
 			###  Count port template, in each template are two ports. Thats why count_port*2
 			count_port = count_port + 1
 		print("The number of ports are:      ", count_port*2)
-		metic = (w_img-h_img)/count_port
+		return loc_full_port
 
-	loc_full_port = sorted(list(OrderedDict.fromkeys(loc_full_port)))
+######	Read the template
+template_green = cv2.imread('',0)
+template_orange = cv2.imread('',0)
+template_dark_orange = cv2.imread('',0)
+template_port = cv2.imread('',0)
+
+######	Store the width (w) and height (h) of the template
+w_green, h_green = template_green.shape[::-1]
+w_orange, h_orange = template_orange.shape[::-1]
+w_dark_orange, h_dark_orange = template_dark_orange.shape[::-1]
+w_port, h_port = template_port.shape[::-1]
+
+######	Specifying (threshold)
+threshold= 0.90
+thresholdport= 0.70
+
+######	Directory with images verify
+img_dir = ''
+data_path = os.path.join(img_dir,'*.jpg')
+files = glob.glob(data_path)
+data = []
+######	Analyzing all the images in the folder
+for f1 in files:
+
+	##### Read each image
+	img = cv2.imread(f1)
+	print("\n", f1) #picture name
+
+	##### Store image data
+	data.append(img)
+	#####	uses a gray filter for easy recognition
+	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY, 0)
+
+	########		 COMPARING THE IMAGE USING TEMPLATE METHOD 			########
+	res_matching_green = cv2.matchTemplate(img_gray,template_green,cv2.TM_CCOEFF_NORMED)
+	res_matching_orange = cv2.matchTemplate(img_gray,template_orange,cv2.TM_CCOEFF_NORMED)
+	res_matching_dark_orange = cv2.matchTemplate(img_gray,template_dark_orange,cv2.TM_CCOEFF_NORMED)
+	res_matching_port = cv2.matchTemplate(img_gray,template_port,cv2.TM_CCOEFF_NORMED)
+
+	##### If you use denoising image:
+	#f1Filtered = cv2.imread(f1)
+	#img = denoising_sharpening(f1Filtered)
+
+	###### Announces every time an image is reviewed
+	print("Image loaded, analyzing patterns ...")
+	###### Gets the position of matching
+	location_green = np.where(res_matching_green >= threshold)
+	location_orange = np.where(res_matching_orange >= threshold)
+	location_dark_orange = np.where(res_matching_dark_orange >= threshold)
+	location_port = np.where(res_matching_port >= thresholdport)
+	# Calling functions
+	X_Green_Filtered0 = color_filter(location_green, w_green, h_green, img, 'GREEN')
+	X_YellowOrange_Filtered0 = color_filter(location_orange, w_orange, h_orange, img, 'ORANGE')
+	X_OrangeOrange_Filtered0 = color_filter(location_dark_orange, w_dark_orange, h_dark_orange, img, 'DARK ORANGE')
+	loc_full_port0 = port_filter(location_port, w_port, h_port, img, 'PORT')
+
+	# Checking empty
+	if X_Green_Filtered0:
+		X_Green_Filtered = list(OrderedDict.fromkeys(X_Green_Filtered0))
+	else:
+		X_Green_Filtered = []
+
+	if X_YellowOrange_Filtered0:
+		X_YellowOrange_Filtered = list(OrderedDict.fromkeys(X_YellowOrange_Filtered0))
+	else:
+		X_YellowOrange_Filtered = []
+
+	if X_OrangeOrange_Filtered0:
+		X_OrangeOrange_Filtered = list(OrderedDict.fromkeys(X_OrangeOrange_Filtered0))
+	else:
+		X_OrangeOrange_Filtered = []
+
+	if loc_full_port0:
+		loc_full_port = list(OrderedDict.fromkeys(loc_full_port0))
+	else:
+		loc_full_port = []
+
 	leds_on_fnd = sorted(list(OrderedDict.fromkeys(X_YellowOrange_Filtered + X_Green_Filtered + X_OrangeOrange_Filtered)))
 
 	print("Positions of the leds ON found", leds_on_fnd)
